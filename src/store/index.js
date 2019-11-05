8840541994b0bfde3bb9723fb9998d341efe8e01
yezy <yezileliilomo@hotmail.com>
2019-11-05 22:02:28 +0300

    :bug:Fix adding setState method on store object

--- src/store/index.js
+++ src/store/index.js
@@ -4,21 +4,24 @@ import { rootReducer } from '../reducers';
 
 
 function configureStore(config){
-    let passedReducer = config.reducer;
-    if(passedReducer === undefined){
+    let reducer = config.reducer;
+    if(reducer === undefined){
         config.reducer = rootReducer
     }
     else{
-        config.reducer = combineReducers({rootReducer, passedReducer});
+        config.reducer = combineReducers({rootReducer, reducer});
     }
 
     let store = prepareStore(config);
 
     // Add setState method on store as altenative to dispatch with action
-    // of type __BASE_ACTIONS__
-    store.constructor.prototype.setState = function (action){
-        action.type = "__BASE_ACTIONS__";
-        this.dispatch(action);
+    // of type BASE_ACTION
+    store.setState = function (baseAction){
+        let action = {
+            type: "BASE_ACTION",
+            payload: baseAction
+        };
+        store.dispatch(action);
     }
     return store
 }
