61fdf43be4a5a4acd339725e238d30a73eaab689
yezy <yezileliilomo@hotmail.com>
2019-12-30 22:35:18 +0300

    :fire:Remove rootReducer on import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
+import { updateReducer } from './reducers';
 import { useLocalState, useGlobalState } from './hooks';
-import { rootReducer, updateReducer } from './reducers';
 
 
 export { 
